dc46d91953ac520792a473781c06eb346f3aa75e
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-02-07 10:39:49 -0300

    cap03 atividade 03

--- Cap03/cap03_atividade02.py
+++ Cap03/cap03_atividade02.py
@@ -9,3 +9,24 @@
   Comandos utilizados:
   If / elif / else, formatação de números com posição de substituição {:.4f}
 """
+import os
+os.system('cls')
+
+medidaCm = float(input('Informe a medida em centímetros: '))
+print('1 - Polegada\n2 - Pé\n3 - Jarda')
+menu=input('Opção: ')
+if (menu=='1'):
+    unidade = 'Polegada'
+    resConversao=medidaCm/2.54
+elif (menu=='2'):
+    unidade = 'Pé'
+    resConversao=medidaCm/30.48
+elif (menu=='3'):
+    unidade = 'Jarda'
+    resConversao=medidaCm/91.44
+else:
+    print('Opção inválida')
+print(f'{medidaCm}cm em {unidade} corresponde a: {resConversao:.4f}' if ('unidade' in globals()) else '')
+print('-------------')
+print('unidade' in globals())
+print(globals())
---
+++ Cap03/cap03_atividade03.py
@@ -0,0 +1,17 @@
+"""
+  Cap03 - Atividade Extra
+  Calcular IMC
+
+  Objetivos:
+  Nesta atividade você vai calcular o IMC a partir de um peso e uma 
+  altura, usará a comando if para mostrar o resultado do calculo do IMC.
+    Ex: IMC = 70 kg / (1,60 m x 1,60 m) = 70 kg / 2,56 m² = 27,3 
+        IMC <18,5kg/m2 - baixo peso
+        IMC >18,5 até 24,9kg/m2 - eutrofia (peso adequado)
+        IMC ≥25 até 29,9kg/m2 - sobrepeso
+        IMC >30,0kg/m2 até 34,9kg/m2 - obesidade grau 1
+        IMC >35kg/m2 até 39,9kg/m2 - obesidade grau 2
+        IMC > 40kg/m2 - obesidade extrema
+  Comandos utilizados:
+  Variáveis, if / elif / else
+"""
