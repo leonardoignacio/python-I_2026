1423182c2a503bd4456377dee41e15e0d9a7d72c
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-02-07 08:29:20 -0300

    cap03 at01

---
+++ Cap03/cap03_atividade01.py
@@ -0,0 +1,13 @@
+"""
+  Cap03 - Atividade 01
+  Verificar Número Par e Impar
+
+  Objetivos:
+  Nesta atividade você vai usar uma estrutura de decisão (if / else) para verificar se um número é par ou ímpar.
+
+  Comandos utilizados:
+  If, operador % (retorna o resto da divisão entre operandos)
+
+"""
+import os
+os.system('cls')
