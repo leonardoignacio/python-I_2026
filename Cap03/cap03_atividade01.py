f814025a93ce0e44f4a36b2fb28d297aa93233f3
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-02-07 08:57:25 -0300

    cap 03 at02

--- Cap03/cap03_atividade01.py
+++ Cap03/cap03_atividade01.py
@@ -11,3 +11,14 @@
 """
 import os
 os.system('cls')
+
+numero = int(input('Informe um número inteiro: '))
+resto = int(numero % 2)
+if (resto == 0):
+    print(f'O número {numero} é: PAR')
+else:
+    print(f'O número {numero} é: IMPAR')
+print()
+print('------------------------')
+print('************************')
+print('Final do Algoritmo!!!!')
---
+++ Cap03/cap03_atividade02.py
@@ -0,0 +1,11 @@
+"""
+  Cap03 - Atividade 02
+  Conversor de Medidas
+
+  Objetivos:
+  Nesta atividade você vai converter um número em centimetros para Polegada, Pé ou Jarda. 
+  Será necessário usar o comando if / elif / else.
+
+  Comandos utilizados:
+  If / elif / else, formatação de números com posição de substituição {:.4f}
+"""
