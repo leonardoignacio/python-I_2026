f8f28d79d2541b6fc25a0258af1b783078ae6730
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-02-14 09:30:45 -0300

    enunciado cap04 at 01

---
+++ Cap04/cap04_atividade01.py
@@ -0,0 +1,10 @@
+"""
+  Cap04 - Atividade 01
+  Tabuada
+
+  Objetivos:
+  Nesta atividade você vai montar uma Tabuada usando a estrutura de Loop do For e range.
+
+  Comandos utilizados:
+  Comandos for e range
+"""
