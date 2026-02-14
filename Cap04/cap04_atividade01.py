c83b55d0f8fcd06fe82a07062af0b6f0346cab04
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-02-14 09:55:41 -0300

    cap04 conclusão da at01 e enunciado da at02

--- Cap04/cap04_atividade01.py
+++ Cap04/cap04_atividade01.py
@@ -8,3 +8,13 @@
   Comandos utilizados:
   Comandos for e range
 """
+from os import system, name
+system('cls') if(name == 'nt') else system('clear')
+print(f'*** Tabuada Simples ***')
+n = int(input('Informe o multiplicador '))
+print(f'*** Tabuada do {n} ***')
+print("-----------------------")
+for i in range(1, 11):
+    print(f'{i}X{n}={n*i}')
+print("-----------------------")
+
---
+++ Cap04/cap04_atividade02.py
@@ -0,0 +1,14 @@
+"""
+  Cap04 - Atividade 02
+  MultiTabuada
+
+  Objetivos:
+  Nesta atividade você vai construir uma multitabuada de duas maneiras, na primeira usando for e na segunda usando for aninhado
+
+  Comandos utilizados:
+   Comandos for range
+"""
+from os import system, name
+system('cls') if(name == 'nt') else system('clear')
+
+print('\n*** MULTI TABUADA 1 ***')
