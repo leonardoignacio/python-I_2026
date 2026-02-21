44e881e0e69e4ba43c82f87a71d08ece00ca1aef
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-02-21 12:01:56 -0300

    pedra, papel e tesoura

--- Cap05/cap05_atividade02.py
+++ Cap05/cap05_atividade02.py
@@ -15,4 +15,23 @@ import random
 
 opcao = 's'
 while opcao.upper()=='S':
-    pass
+    system('cls') if(name == 'nt') else system('clear')
+    opcoes=("Pedra", "Papel", "Tesoura")
+    print("Escolha a sua jogada: ")
+    for i, elemento in enumerate(opcoes):
+      print(f'{i+1} - {elemento}')
+    jogador=int(input())-1
+    cpu = random.randint(0,2)
+    jM="Parabéns!!! Você venceu :)"
+    eM="Deu empate, só perdeu tempo /:"
+    cpuM="Deu RED. A CPU venceu :("
+    resultado = (
+                #   0   1   2
+                  (eM, jM,cpuM),#0
+                  (cpuM,eM, jM),#1
+                  (jM, cpuM,eM) #2
+                )
+    print(f'Você escolheu {opcoes[jogador]}')
+    print(f'A CPU escolheu {opcoes[cpu]}')
+    print(resultado[cpu][jogador])
+    opcao = input("Digite S para continuar ou qualquer tecla para fizalizar.")  
