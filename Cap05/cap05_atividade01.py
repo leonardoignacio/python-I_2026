4aa943d3126982218db3affc61e142bc0f77e219
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-02-21 09:59:03 -0300

    cap05 at02 enunciado e introdução

--- Cap05/cap05_atividade01.py
+++ Cap05/cap05_atividade01.py
@@ -14,3 +14,19 @@ system('cls') if(name == 'nt') else system('clear')
 unidades = ('zero','um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove')
 dezenas = ('dez','onze', 'doze', 'trêze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove')
 rasos=('','','vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta','oitenta', 'noventa')
+
+numero = input("Informe um número entre 0 e 99 para ser convertido para texto: ")
+numero=int(numero)
+if numero<10:
+    numTexto = unidades[numero]
+elif numero <20:
+    numTexto = dezenas[numero-10]
+elif numero <100:
+    u = numero % 10
+    d = int(numero/10)
+    numTexto = rasos[d] 
+    if u>0:
+        numTexto += f' e  {unidades[u]}'
+else:
+    numTexto = f'O valor {numero} é inválido, tente novamente mais tarde!'
+print(numTexto)
---
+++ Cap05/cap05_atividade02.py
@@ -0,0 +1,18 @@
+"""
+  Cap05 - Atividade 02
+  Jogo: Papel, pedra e Tesoura
+
+  Objetivos:
+  Nesta atividade você vai criar um jogo usando tupla e tupla multi-dimensional.
+
+  Comandos utilizados:
+  Tupla, Tupla Multi-Dimensional, biblioteca random e randint
+"""
+
+from os import system, name
+# biblioteca random
+import random
+
+opcao = 's'
+while opcao.upper()=='S':
+    pass
