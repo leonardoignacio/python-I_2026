0957c82bf12060e4bbb4ca7ff3a375bf785ae773
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-02-21 08:47:50 -0300

    Introdução cap05 atividade01

---
+++ Cap05/cap05_atividade01.py
@@ -0,0 +1,13 @@
+"""
+  Cap05 - Atividade 01
+  Número por Extenso
+  Objetivos:
+  Nesta atividade você vai escrever um número por extenso, para isto usará uma tupla. 
+  A tupla é um array que contem dados que não pode ser alterados.
+  Comandos utilizados:
+  Tupla, operadores / e %
+"""
+from os import system, name
+system('cls') if(name == 'nt') else system('clear')
+# tupla é criada com () e não pode ter seus dados alterados
+# lista é criada com [] e pode ter seus dados alterados
