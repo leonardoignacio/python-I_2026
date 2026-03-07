8c9037cd3c4336c29378ea37fffff9fe51f294ec
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-03-07 12:09:51 -0300

    Cap07 POO extras

---
+++ Cap07/Produto/main.py
@@ -0,0 +1,6 @@
+from classe import Produto
+notebook = Produto('Notebook Dell i7', 5800.90)
+input('Pressione qualquer tecla para ver a oferta: ')
+notebook.verProduto()
+print()
+
---
+++ Cap07/ProdutoEncapsulamento/main.py
@@ -0,0 +1,10 @@
+from classe import Produto
+notebook = Produto('Notebook Dell i7', 3800, 5800)
+print("Mudar preço para 1000")
+notebook.precoVenda=100
+print("Mudar preço para 5100")
+notebook.precoVenda=5100
+print('----')
+
+
+
