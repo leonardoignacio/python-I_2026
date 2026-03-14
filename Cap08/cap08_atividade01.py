6ae199fef815cd882bc8a29da0d4e15c2d657f6e
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-03-14 09:53:37 -0300

    Ativivdade 02

--- Cap08/cap08_atividade01.py
+++ Cap08/cap08_atividade01.py
@@ -4,8 +4,7 @@
 
   Objetivos:
   Nesta atividade você vai ler um arquivo no formato CSV, verificar as opções de 
-  enconding (codificação de caracteres) e separar os arquivo em uma lista para ler 
-  as informações de cada coluna.
+  enconding (codificação de caracteres) e separar os arquivo em uma lista para ler as informações de cada coluna.
 
   Comandos utilizados:
   Biblioteca os, comando open, path, lista e for
@@ -13,3 +12,29 @@
 import os.path, datetime
 from os import system, name
 system('cls') if(name == 'nt') else system('clear')
+arquivo = 'produtos.csv'
+if (os.path.isfile(arquivo)):
+    produtos = open(arquivo, 'r', encoding="utf-8")
+    tamanho = os.path.getsize(arquivo)
+    dtModificacao = os.path.getmtime(arquivo)
+    print('Tamanho do arquivo (bytes):', tamanho)
+    print('Data de Modificação:', datetime.datetime.fromtimestamp(dtModificacao))
+    listaProdutos = []
+    for line in produtos:
+      colunas = line.strip().split(";")
+      #colunas do tipo int
+      colunas[0]=int(colunas[0])
+      colunas[2]=int(colunas[2])
+      colunas[4]=int(colunas[4])
+      colunas[5]=int(colunas[5])
+      colunas[6]=int(colunas[6])
+      colunas[7]=int(colunas[7])
+      #colunas do tipo float
+      colunas[8]=float(colunas[8])
+      colunas[9]=float(colunas[9])
+      listaProdutos.append(colunas)
+    produtos.close()
+    for prod in listaProdutos:
+      print(prod)
+else:
+    print('Arquivo inválido')
