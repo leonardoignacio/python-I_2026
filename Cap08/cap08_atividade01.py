f24e0e4f0fac92c9b5a8d1b27d931740307c2658
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-03-14 08:39:13 -0300

    cap08 intrudução

---
+++ Cap08/cap08_atividade01.py
@@ -0,0 +1,15 @@
+"""
+  Cap08 - Atividade 01
+  Abrir um arquivo 
+
+  Objetivos:
+  Nesta atividade você vai ler um arquivo no formato CSV, verificar as opções de 
+  enconding (codificação de caracteres) e separar os arquivo em uma lista para ler 
+  as informações de cada coluna.
+
+  Comandos utilizados:
+  Biblioteca os, comando open, path, lista e for
+"""
+import os.path, datetime
+from os import system, name
+system('cls') if(name == 'nt') else system('clear')
